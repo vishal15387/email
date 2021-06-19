09ab5000386d1b8f08ce713db20098dc6175b3b8
vishal15387 <86072429+vishal15387@users.noreply.github.com>
2021-06-19 12:59:03 +0530

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -26,7 +26,7 @@ $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
     
-$to = "vishalrathod86@gmsil.com";//<== update the email address
+$to = "vishalrathod86@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
